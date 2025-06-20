5020321bec80ccf37a5f719ecc31307e9782b715
Viren Nadkarni <viren.nadkarni@gmail.com>
2025-06-20 18:07:10 +0530

    Add sample

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# sample-codepipeline-compile-test-publish
+# sample-codepipeline-compile-publish
 
-This sample creats a CodePipeline pipeline that compiles a Rust binary, runs tests and publishes the binary to S3.
+This sample creates a CodePipeline pipeline that builds the [fzf](https://github.com/junegunn/fzf/) project, runs tests and publishes the compiled binary to S3.
+
+Start LocalStack using any supported method.
+
+You must set `CODEPIPELINE_GH_TOKEN` environment config option to the [PAT token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-fine-grained-personal-access-token). GitHub requires this to [download](https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#download-a-repository-archive-zip) even public repositories.
+
+Run the following script which will create all prerequisite resource and the pipeline. It will also download the compiled binary from the S3 bucket and execute it.
+
+```
+bash run.sh
+```
+
+The pipeline is structured as follows:
+
+```mermaid
+flowchart LR
+        A("CodeStarSourceConnection: Download source code")
+        A --> B("CodeBuild: Compile")
+        B --> C("S3: Upload binary")
+```
---
+++ run.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+# Create service IAM roles
+awslocal iam create-role --role-name demo-role --assume-role-policy-document file://role.json
+awslocal iam put-role-policy --role-name demo-role --policy-name demo-policy --policy-document file://policy.json
+
+# Create CodeConnection connection
+awslocal codeconnections create-connection --connection-name demo-connection --provider-type GitHub
+
+# Create CodeBuild project
+awslocal codebuild create-project \
+    --name demo-compile \
+    --source type=CODEPIPELINE,buildspec="$(cat buildspec.yaml)" \
+    --artifacts type=CODEPIPELINE \
+    --environment type=LINUX_CONTAINER,image=aws/codebuild/standard:5.0,computeType=BUILD_GENERAL1_SMALL \
+    --service-role arn:aws:iam::000000000000:role/demo-role
+
+# Create pipeline artifact bucket
+awslocal s3 mb s3://artifact-bucket
+
+# Create pipeline output bucket
+awslocal s3 mb s3://output-bucket
+
+# Create pipeline
+awslocal codepipeline create-pipeline --pipeline file://./pipeline.json
+
+# Check pipeline status
+awslocal codepipeline list-pipeline-executions --pipeline-name demo-pipeline
+
+# Wait for pipeline to finish
+status=
+until [[ $status =~ Succeeded ]]; do
+    status=$(awslocal codepipeline list-pipeline-executions --pipeline-name demo-pipeline --output text --query pipelineExecutionSummaries[0].status)
+    sleep 5
+done
+
+# Retrieve build
+awslocal s3 cp s3://output-bucket/fzf/fzf .
+
+# Test build
+file fzf
+chmod +x fzf
+./fzf --version
