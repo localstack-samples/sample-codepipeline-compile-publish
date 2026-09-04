4fb2bff5824658f2119ce344f9a070256a2d7335
Harsh Mishra <erbeusgriffincasper@gmail.com>
2026-09-04 19:26:01 +0530

    Migrate from the localstack CLI to lstk

--- 1-codebuild-compile-s3-upload/run.sh
+++ 1-codebuild-compile-s3-upload/run.sh
@@ -3,14 +3,14 @@
 set -xeuo pipefail
 
 # Create service IAM roles
-awslocal iam create-role --role-name demo-role --assume-role-policy-document file://role.json
-awslocal iam put-role-policy --role-name demo-role --policy-name demo-policy --policy-document file://policy.json
+lstk aws iam create-role --role-name demo-role --assume-role-policy-document file://role.json
+lstk aws iam put-role-policy --role-name demo-role --policy-name demo-policy --policy-document file://policy.json
 
 # Create CodeConnection connection
-awslocal codeconnections create-connection --connection-name demo-connection --provider-type GitHub
+lstk aws codeconnections create-connection --connection-name demo-connection --provider-type GitHub
 
 # Create CodeBuild project
-awslocal codebuild create-project \
+lstk aws codebuild create-project \
     --name demo-compile \
     --source type=CODEPIPELINE,buildspec="$(cat buildspec.yaml)" \
     --artifacts type=CODEPIPELINE \
@@ -18,26 +18,26 @@ awslocal codebuild create-project \
     --service-role arn:aws:iam::000000000000:role/demo-role
 
 # Create pipeline artifact bucket
-awslocal s3 mb s3://artifact-bucket
+lstk aws s3 mb s3://artifact-bucket
 
 # Create pipeline output bucket
-awslocal s3 mb s3://output-bucket
+lstk aws s3 mb s3://output-bucket
 
 # Create pipeline
-awslocal codepipeline create-pipeline --pipeline file://./pipeline.json
+lstk aws codepipeline create-pipeline --pipeline file://./pipeline.json
 
 # Check pipeline status
-awslocal codepipeline list-pipeline-executions --pipeline-name demo-pipeline
+lstk aws codepipeline list-pipeline-executions --pipeline-name demo-pipeline
 
 # Wait for pipeline to finish
 status=
 until [[ $status =~ Succeeded ]]; do
-    status=$(awslocal codepipeline list-pipeline-executions --pipeline-name demo-pipeline --output text --query pipelineExecutionSummaries[0].status)
+    status=$(lstk aws codepipeline list-pipeline-executions --pipeline-name demo-pipeline --output text --query pipelineExecutionSummaries[0].status)
     sleep 5
 done
 
 # Retrieve build
-awslocal s3 cp s3://output-bucket/fzf/fzf .
+lstk aws s3 cp s3://output-bucket/fzf/fzf .
 
 # Try build
 file fzf
